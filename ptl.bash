f95e290bfa6e980fc5ff705454516311b442fe12
vaibhavdih <40610705+vaibhavdih@users.noreply.github.com>
2025-07-11 13:20:30 +0530

    Update ptl.bash

--- ptl.bash
+++ ptl.bash
@@ -4,14 +4,14 @@
 set -e
 
 # Update and install dependencies
-apt update
-apt install -y python3 python3-pip python3-venv git nodejs npm yarn mariadb-server redis-server curl wkhtmltopdf
+# apt update
+# apt install -y python3 python3-pip python3-venv git nodejs npm yarn mariadb-server redis-server curl wkhtmltopdf
 
-# Install Frappe Bench
-pip install frappe-bench==5.25.0
+# # Install Frappe Bench
+# pip install frappe-bench==5.25.0
 
-# Verify bench installation
-bench --version
+# # Verify bench installation
+# bench --version
 
 
 echo "Frappe Bench setup complete in ~$pwd"
